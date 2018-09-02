b6bf086a03084e473fc296ef3e28f7deaf68716c
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2018-09-02 22:21:59 +0100

    Adding to github for safe keeping

---
+++ backup/menu.js
@@ -0,0 +1,12 @@
+document.write(' \
+  <td colspan="2" style="height: 300px" align="left"> \
+    <div class="dropdown"> <button onclick="myFunction()" class="dropbtn"> \
+	<div class="menuline"></div> \
+	<div class="menuline"></div> \
+	<div class="menuline"></div> \
+      </button> \
+      <div id="myDropdown" class="dropdown-content"> <a href="index.html">Home</a> \
+	<a href="courselist.html">Course List</a> <a href="where.html">How \
+	  to Find Us</a> <a href="contact.html">Contact Us</a></div> \
+    </div> \
+  </td>');
